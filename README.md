4f4c97131e32712cd475480cd08ca35d8af52fe1
moses kiboma <kibomamoses100@gmail.com>
2021-11-17 21:14:11 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+<h1 align="center"> Livewire Ecommerce Platform</h1>
+
+<p align="center">This is a e-commerce platform I made powered Laravel 8</p>
---
+++ resources/views/livewire/product/create.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+    <div>
+        <h2 class="text-center">Add new product</h2>
+    </div>
+@endsection
